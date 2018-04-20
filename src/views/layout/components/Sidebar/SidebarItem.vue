1308be5536758396d5840c2753e65e50023ebdf6
Unknown <fengwu2006@gmail.com>
2018-04-20 19:57:01 +0800

    save

--- src/views/layout/components/Sidebar/SidebarItem.vue
+++ src/views/layout/components/Sidebar/SidebarItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="menu-wrapper">
     <template v-for="item in routes" v-if="!item.hidden&&item.children">
-      <router-link v-if="hasOneShowingChildren(item.children) && !item.children[0].children&&!item.alwaysShow" :to="item.path+'/'+item.children[0].path"
+      <router-link v-if="hasOneShowingChildren(item.children)" :to="item.path+'/'+item.children[0].path"
         :key="item.children[0].name">
         <el-menu-item :index="item.path+'/'+item.children[0].path" :class="{'submenu-title-noDropdown':!isNest}">
           <svg-icon v-if="item.children[0].meta&&item.children[0].meta.icon" :icon-class="item.children[0].meta.icon"></svg-icon>
@@ -55,7 +55,7 @@ export default {
 
         return true
       }
-      
+
       return false
     },
     generateTitle
