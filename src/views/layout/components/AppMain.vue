e962db913ac6a5132661fe06f84eac7516af7854
Unknown <fengwu2006@gmail.com>
2018-04-20 16:13:38 +0800

    save

--- src/views/layout/components/AppMain.vue
+++ src/views/layout/components/AppMain.vue
@@ -2,9 +2,7 @@
   <section class="app-main" style="min-height: 100%">
     <navbar></navbar>
     <transition name="fade" mode="out-in">
-      <keep-alive :include="cachedViews">
         <router-view></router-view>
-      </keep-alive>
     </transition>
   </section>
 </template>
@@ -15,10 +13,5 @@
   export default {
     name: 'AppMain',
     components: { Navbar },
-    computed: {
-      cachedViews() {
-        return this.$store.state.tagsView.cachedViews
-      }
-    }
   }
 </script>
