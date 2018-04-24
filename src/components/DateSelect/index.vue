6e0c1cf7e02b775b6f4044628ffef88af0dfb94a
Unknown <fengwu2006@gmail.com>
2018-04-24 15:55:25 +0800

    save

--- src/components/DateSelect/index.vue
+++ src/components/DateSelect/index.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="dateselect">
-    <span>统计时间</span>
-
+    <span class="title">统计时间</span>
+    <el-radio-group v-model="days">
+      <el-radio-button label="今天"></el-radio-button>
+      <el-radio-button label="本周"></el-radio-button>
+      <el-radio-button label="本月"></el-radio-button>
+      <el-radio-button label="本年"></el-radio-button>
+    </el-radio-group>
+    <div class="intervalselect">
+      <span>选择时间:</span>
+      <el-date-picker v-model="dayinterval" type="datetimerange" placeholder="选择时间范围"></el-date-picker>
+    </div>
   </div>
 </template>
 
 <script>
 
   export default {
-
+    data () {
+      return {
+        days: '本周',
+        dayinterval: [new Date(2018, 10, 10, 10, 10), new Date(2018, 10, 11, 10, 10)],
+      };
+    }
   };
 </script>
 
@@ -16,6 +30,25 @@
 
   .dateselect {
 
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    .title {
+
+      margin-right: 1rem;
+    }
+
+    .intervalselect {
+
+      margin-left: 3rem;
+    }
+
+    .intervalselect > span {
+
+      margin-left: 1rem;
+      margin-right: 1rem;
+    }
   }
 
 </style>
--- src/views/messagepush/areapush/index.vue
+++ src/views/messagepush/areapush/index.vue
@@ -1,22 +1,87 @@
 <template>
   <transition name="fade" mode="out-in">
-    <div class="header">
-      <date-select></date-select>
-      <div class="operatemenu">
-        <el-input ></el-input><el-button type="primary">查询</el-button>
+    <div>
+      <div class="header">
+        <date-select></date-select>
+        <div class="operatemenu">
+          <el-input placeholder="输入主题查询"></el-input><el-button type="primary">查询</el-button>
+        </div>
+      </div>
+      <div class="table">
+        <el-table :data="tableData" :header-cell-style="headercellstyle">
+          <el-table-column prop="title" label="内容主题" width="400"></el-table-column>
+          <el-table-column prop="type" label="消息类型" width="150"></el-table-column>
+          <el-table-column prop="send" label="发送方式" width="150"></el-table-column>
+          <el-table-column prop="tag" label="发布状态" width="100"
+                           :filters="[{ text: '已发布', value: 'alreadysend' }, { text: '未发布', value: 'unsend' }]"
+                           :filter-method="filterTag"
+                           :filter-multiple="false">
+            <template slot-scope="scope">
+              <el-tag :type="scope.row.tag === '已发布' ? 'primary' : 'success'" close-transition>{{ scope.row.tag }}</el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column prop="strategy" label="发送策略" width="150"></el-table-column>
+          <el-table-column prop="time" label="计划发起时间" width="250"></el-table-column>
+          <el-table-column label="操作">
+            <template slot-scope="scope">
+              <el-button size="mini" @click="handleDetail(scope.$index, scope.row)">发布</el-button>
+              <el-button size="mini" @click="handleManager(scope.$index, scope.row)">修改</el-button>
+              <el-button size="mini" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <div class="pagination">
+        <page-widget :total="100" :pagesizes="[10, 20, 40, 50]"></page-widget>
       </div>
     </div>
   </transition>
 </template>
 
 <script>
+  import PageWidget from '@/components/PageWidget'
   import DateSelect from '@/components/DateSelect'
 
   export default {
 
-    components: { DateSelect },
+    components: { DateSelect, PageWidget },
+
+    data() {
+      return {
+        tableData: [{
+          title:'物业管理月度业主投票统计',
+          type:'投票',
+          send:'后台',
+          status:'未发布',
+          strategy: '定时发送',
+          time: '2018-10-05 9:30',
+          tag:'未发布'
+        }],
+      }
+    },
+
+    methods: {
+      headercellstyle({row, rowIndex, columnIndex}) {
+
+        return {textAlign: 'left', backgroundColor:'#d0d5e5'}
+      },
+      handleDetail(index, row) {
 
 
+      },
+      handleManager(index, row) {
+
+
+      },
+      handleDelete(index, row) {
+
+
+      },
+      filterTag(value, row) {
+
+        return row.tag === value;
+      },
+    }
   }
 </script>
 
@@ -25,7 +90,7 @@
   .header {
 
     display: flex;
-    justify-content: space-around;
+    justify-content: space-between;
 
     .dateselect {
 
@@ -39,6 +104,26 @@
       justify-content: space-around;
       align-items: center;
     }
+
+    .el-button {
+
+      margin-left: 1rem;
+    }
+  }
+
+  .table {
+
+    margin-top: 1rem;
+  }
+
+  .headerrowclass {
+
+    background-color: red !important;
+  }
+
+  .pagination {
+
+    margin-top: 1rem;
   }
 
 </style>
