06d4403f8b566ed1da782a7cb4db98bfde0ac3b5
Unknown <fengwu2006@gmail.com>
2018-04-25 09:41:27 +0800

    save

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,42 +1,40 @@
 <template>
-  <div id="login">
-    <div v-show="false">
-      <img class="cloud cloud1" :src="img1">
-      <img class="cloud cloud2" :src="img2">
-      <img class="cloud cloud3" :src="img3">
-      <img class="cloud cloud4" :src="img4">
-      <img class="cloud cloud5" :src="img5">
-      <img class="cloud cloud6" :src="img6">
+  <div class="login">
+    <div class="backgroundimg">
+      <img class="cloud1" :src="img1">
+      <img class="cloud2" :src="img2">
+      <img class="cloud3" :src="img3">
+      <img class="cloud4" :src="img4">
+      <img class="cloud5" :src="img5">
+      <img class="cloud6" :src="img6">
       <img class="city" :src="logo">
     </div>
     <div class="main">
-      <div class="content">
+      <el-form class="content" :model="loginForm" :rules="loginRules" ref="loginForm">
         <div class="icon"/>
         <div class="title"/>
-        <div v-if="isnull" class="text-danger">验证码不能为空</div>
-        <div class="form-group">
-          <input type="text" placeholder="请输入用户名">
+        <div class="inputgroup">
+          <input placeholder="请输入用户名"/>
         </div>
-        <div class="form-group">
-          <input type="password" placeholder="请输入密码">
+        <div class="inputgroup">
+          <input type="password"  placeholder="请输入密码"/>
         </div>
-        <div class="form-group">
-          <input type="text" placeholder="请输入验证码" alt="验证码">
-          <img src="http://120.77.201.142:9507/jsis_3.4.1/randomimage.servlet?falg=1524540618134" class="img-random  "
-               title="点击换一张">
+        <div class="inputgroup">
+          <div class="inputcodegroup">
+            <input type="code" placeholder="请输入验证码" alt="验证码">
+            <img src="http://120.77.201.142:9507/jsis_3.4.1/randomimage.servlet?falg=1524540618134" class="codeimg">
+          </div>
         </div>
-        <div class="form-group">
-          <a class="btn text-center">登录</a>
+        <div class="confirm">
+          <el-button type="danger" @click.native.prevent="handleLogin">登 陆</el-button>
         </div>
-      </div>
+      </el-form>
     </div>
   </div>
 </template>
 
 <script>
 
-  import loginimg from '@/assets/loginimg/community-icon.png'
-  import logintitle from '@/assets/loginimg/title.png'
   import img1 from '@/assets/loginimg/cloud1.png'
   import img2 from '@/assets/loginimg/cloud2.png'
   import img3 from '@/assets/loginimg/cloud3.png'
@@ -45,13 +43,36 @@
   import img6 from '@/assets/loginimg/cloud6.png'
   import logo from '@/assets/loginimg/logo.png'
 
+  import { isvalidUsername } from '@/utils/validate'
+
+  const validateUsername = (rule, value, callback) => {
+
+    if (!isvalidUsername(value)) {
+
+      callback(new Error('Please enter the correct user name'))
+    }
+    else {
+
+      callback()
+    }
+  }
+
+  const validatePassword = (rule, value, callback) => {
+
+    if (value.length < 6) {
+
+      callback(new Error('The password can not be less than 6 digits'))
+    }
+    else {
+
+      callback()
+    }
+  }
 
   export default {
     data() {
       return {
         isnull: false,
-        loginimg,
-        logintitle,
         img1,
         img2,
         img3,
@@ -59,7 +80,33 @@
         img5,
         img6,
         logo,
+        loginForm: {
+          username: 'admin',
+          password: '1111111'
+        },
+        loginRules: {
+          username: [{ required: true, trigger: 'blur', validator: validateUsername }],
+          password: [{ required: true, trigger: 'blur', validator: validatePassword }]
+        },
       }
+    },
+    methods: {
+      handleLogin() {
+        this.$refs.loginForm.validate(valid => {
+          if (valid) {
+            this.loading = true
+            this.$store.dispatch('LoginByUsername', this.loginForm).then(() => {
+              this.loading = false
+              this.$router.push({ path: '/messagepush' })
+            }).catch(() => {
+              this.loading = false
+            })
+          } else {
+            console.log('error submit!!')
+            return false
+          }
+        })
+      },
     }
   }
 
@@ -67,24 +114,100 @@
 
 <style rel="stylesheet/scss" lang="scss" scoped>
 
+  .login {
+
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+
   .main {
 
-    margin: 10rem auto;width: 40%; max-width: 400px;
-    background: red;
+    margin: 10rem auto;
+    background: white;
+    position: absolute;
+    z-index: 999;
+    left: 0;
+    right: 0;
+    width: 40%;
+    max-width: 300px;
+    display: flex;
+    box-shadow: 0 4px 15px 0 rgba(0, 0, 0, 0.20);
+    justify-content: center;
   }
 
   .content {
 
     width: 80%;
-    margin: auto;
   }
 
   .icon {
 
     width: 45px;
     height: 45px;
-    margin-top: 2rem;
-    background: url("/static/loginimg/community-icon.png") no-repeat center/100% 100%;
+    margin: 1rem auto;
+    background: url("/static/loginimg/community-icon.png") no-repeat center/100%;
+  }
+
+  .title {
+
+    width: 200px;
+    height: 30px;
+    margin: 1rem auto;
+    background: url("/static/loginimg/title.png") no-repeat center/100%;
+  }
+
+  .inputgroup {
+
+    margin-top: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 1px solid #d6dadd;
+    background: white;
+
+    input {
+
+      line-height: 1.5rem;
+      width: 90%;
+      margin: 0.5rem auto;
+      border: none;
+      outline: none;
+      font-size: 0.8rem;
+    }
+  }
+
+  .inputcodegroup {
+
+    width: 90%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .code {
+
+      width: 40%;
+    }
+
+    .codeimg {
+
+      width: 40%;
+    }
+  }
+
+  .confirm {
+
+    width: 100%;
+    height: 2rem;
+    margin: 2rem auto;
+
+    .el-button {
+
+      width: 100%;
+      line-height: 2rem;
+      background: #FF955B;
+      border: none;
+    }
   }
 
   .city {
@@ -96,11 +219,7 @@
     display: block;
     z-index: 999;
     animation: city 5s linear infinite;
-    -moz-animation: city 5s linear infinite;
-    -webkit-animation: city 5s linear infinite;
-    -o-animation: city 5s linear infinite;
   }
-
   @keyframes city {
     0% {
       left: 0%;
@@ -119,157 +238,26 @@
     }
   }
 
-  @-webkit-keyframes city {
-    0% {
-      left: 0%;
-    }
-    20% {
-      left: 1%;
-    }
-    50% {
-      left: 2%;
-    }
-    80% {
-      left: 1%;
-    }
-    100% {
-      left: 0%;
-    }
-  }
-
-  @-moz-keyframes city {
-    0% {
-      left: 0%;
-    }
-    20% {
-      left: 1%;
-    }
-    50% {
-      left: 2%;
-    }
-    80% {
-      left: 1%;
-    }
-    100% {
-      left: 0%;
-    }
-  }
-
-  @-o-keyframes city {
-    0% {
-      left: 0%;
-    }
-    20% {
-      left: 1%;
-    }
-    50% {
-      left: 2%;
-    }
-    80% {
-      left: 1%;
-    }
-    100% {
-      left: 0%;
-    }
-  }
-
-  .cloud {
+  .backgroundimg {
 
     position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+  }
+
+  .backgroundimg > img {
+
+    position: absolute;
+    z-index: 1;
   }
 
   .cloud1 {
     top: 8%;
     left: 0%;
-    z-index: 0;
     animation: cloud1 5s infinite linear;
-    -moz-animation: cloud1 5s infinite linear;
-    -webkit-animation: cloud1 5s infinite linear;
-    -o-animation: cloud1 5s infinite linear;
   }
-
   @keyframes cloud1 {
-    0% {
-      transform: rotate(0deg);
-      left: 0%;
-    }
-    25% {
-      transform: rotate(10deg);
-      left: 2%;
-    }
-    50% {
-      transform: rotate(15deg);
-      left: 3%;
-    }
-    55% {
-      transform: rotate(10deg);
-      left: 3.5%;
-    }
-    70% {
-      transform: rotate(5deg);
-      left: 2%;
-    }
-    100% {
-      transform: rotate(0deg);
-      left: 0%;
-    }
-  }
-
-  @-webkit-keyframes cloud1 {
-    0% {
-      transform: rotate(0deg);
-      left: 0%;
-    }
-    25% {
-      transform: rotate(10deg);
-      left: 2%;
-    }
-    50% {
-      transform: rotate(15deg);
-      left: 3%;
-    }
-    55% {
-      transform: rotate(10deg);
-      left: 3.5%;
-    }
-    70% {
-      transform: rotate(5deg);
-      left: 2%;
-    }
-    100% {
-      transform: rotate(0deg);
-      left: 0%;
-    }
-  }
-
-  @-moz-keyframes cloud1 {
-    0% {
-      transform: rotate(0deg);
-      left: 0%;
-    }
-    25% {
-      transform: rotate(10deg);
-      left: 2%;
-    }
-    50% {
-      transform: rotate(15deg);
-      left: 3%;
-    }
-    55% {
-      transform: rotate(10deg);
-      left: 3.5%;
-    }
-    70% {
-      transform: rotate(5deg);
-      left: 2%;
-    }
-    100% {
-      transform: rotate(0deg);
-      left: 0%;
-    }
-  }
-
-  @-o-keyframes cloud1 {
     0% {
       transform: rotate(0deg);
       left: 0%;
@@ -299,95 +287,9 @@
   .cloud2 {
     top: 3%;
     left: 15%;
-    z-index: 0;
     animation: cloud2 10s infinite linear;
-    -moz-animation: cloud2 10s infinite linear;
-    -webkit-animation: cloud2 10s infinite linear;
-    -o-animation: cloud2 10s infinite linear;
   }
-
   @keyframes cloud2 {
-    0% {
-      transform: rotate(5deg);
-      left: 15%;
-    }
-    25% {
-      transform: rotate(0deg);
-      left: 30%;
-    }
-    50% {
-      transform: rotate(-5deg);
-      left: 45%;
-    }
-    65% {
-      transform: rotate(-10deg);
-      left: 45%;
-    }
-    80% {
-      transform: rotate(-5 eg);
-      left: 30%;
-    }
-    100% {
-      transform: rotate(5deg);
-      left: 15%;
-    }
-  }
-
-  @-webkit-keyframes cloud2 {
-    0% {
-      transform: rotate(5deg);
-      left: 15%;
-    }
-    25% {
-      transform: rotate(0deg);
-      left: 30%;
-    }
-    50% {
-      transform: rotate(-5deg);
-      left: 45%;
-    }
-    65% {
-      transform: rotate(-10deg);
-      left: 45%;
-    }
-    80% {
-      transform: rotate(-5 eg);
-      left: 30%;
-    }
-    100% {
-      transform: rotate(5deg);
-      left: 15%;
-    }
-  }
-
-  @-moz-keyframes cloud2 {
-    0% {
-      transform: rotate(5deg);
-      left: 15%;
-    }
-    25% {
-      transform: rotate(0deg);
-      left: 30%;
-    }
-    50% {
-      transform: rotate(-5deg);
-      left: 45%;
-    }
-    65% {
-      transform: rotate(-10deg);
-      left: 45%;
-    }
-    80% {
-      transform: rotate(-5 eg);
-      left: 30%;
-    }
-    100% {
-      transform: rotate(5deg);
-      left: 15%;
-    }
-  }
-
-  @-o-keyframes cloud2 {
     0% {
       transform: rotate(5deg);
       left: 15%;
@@ -418,81 +320,8 @@
     top: 35%;
     left: 4%;
     animation: cloud5 3s infinite linear;
-    -moz-animation: cloud3 3s infinite linear;
-    -webkit-animation: cloud3 3s infinite linear;
-    -o-animation: cloud3 3s infinite linear;
   }
-
   @keyframes cloud3 {
-    0% {
-      transform: rotate(0deg);
-      top: 35%;
-    }
-    22.5% {
-      transform: rotate(2deg);
-      top: 38%;
-    }
-    45% {
-      transform: rotate(2deg);
-      top: 40%;
-    }
-    67.5% {
-      transform: rotate(0deg);
-      top: 38%;
-    }
-    100% {
-      transform: rotate(2deg);
-      top: 35%;
-    }
-  }
-
-  @-webkit-keyframes cloud3 {
-    0% {
-      transform: rotate(0deg);
-      top: 35%;
-    }
-    22.5% {
-      transform: rotate(2deg);
-      top: 38%;
-    }
-    45% {
-      transform: rotate(2deg);
-      top: 40%;
-    }
-    67.5% {
-      transform: rotate(0deg);
-      top: 38%;
-    }
-    100% {
-      transform: rotate(2deg);
-      top: 35%;
-    }
-  }
-
-  @-moz-keyframes cloud3 {
-    0% {
-      transform: rotate(0deg);
-      top: 35%;
-    }
-    22.5% {
-      transform: rotate(2deg);
-      top: 38%;
-    }
-    45% {
-      transform: rotate(2deg);
-      top: 40%;
-    }
-    67.5% {
-      transform: rotate(0deg);
-      top: 38%;
-    }
-    100% {
-      transform: rotate(2deg);
-      top: 35%;
-    }
-  }
-
-  @-o-keyframes cloud3 {
     0% {
       transform: rotate(0deg);
       top: 35%;
@@ -519,81 +348,8 @@
     top: 29%;
     left: 68%;
     animation: cloud4 5s infinite linear;
-    -moz-animation: cloud4 5s infinite linear;
-    -webkit-animation: cloud4 5s infinite linear;
-    -o-animation: cloud4 5s infinite linear;
-  }
-
+   }
   @keyframes cloud4 {
-    0% {
-      transform: rotate(0deg);
-      left: 68%;
-    }
-    35% {
-      transform: rotate(20deg);
-      left: 68%;
-    }
-    60% {
-      transform: rotate(0deg);
-      left: 68%;
-    }
-    85% {
-      transform: rotate(-20deg);
-      left: 68%;
-    }
-    100% {
-      transform: rotate(0deg);
-      left: 68%;
-    }
-  }
-
-  @-webkit-keyframes cloud4 {
-    0% {
-      transform: rotate(0deg);
-      left: 68%;
-    }
-    35% {
-      transform: rotate(20deg);
-      left: 68%;
-    }
-    60% {
-      transform: rotate(0deg);
-      left: 68%;
-    }
-    85% {
-      transform: rotate(-20deg);
-      left: 68%;
-    }
-    100% {
-      transform: rotate(0deg);
-      left: 68%;
-    }
-  }
-
-  @-moz-keyframes cloud4 {
-    0% {
-      transform: rotate(0deg);
-      left: 68%;
-    }
-    35% {
-      transform: rotate(20deg);
-      left: 68%;
-    }
-    60% {
-      transform: rotate(0deg);
-      left: 68%;
-    }
-    85% {
-      transform: rotate(-20deg);
-      left: 68%;
-    }
-    100% {
-      transform: rotate(0deg);
-      left: 68%;
-    }
-  }
-
-  @-o-keyframes cloud4 {
     0% {
       transform: rotate(0deg);
       left: 68%;
@@ -620,93 +376,8 @@
     left: 80%;
     top: 30%;
     animation: cloud5 5s infinite linear;
-    -moz-animation: cloud5 5s infinite linear;
-    -webkit-animation: cloud5 5s infinite linear;
-    -o-animation: cloud5 5s infinite linear;
   }
-
   @keyframes cloud5 {
-    0% {
-      width: 239px;
-      height: 209px
-    }
-    25% {
-      width: 249px;
-      height: 219px
-    }
-    50% {
-      width: 259px;
-      height: 229px
-    }
-    55% {
-      width: 259px;
-      height: 229px
-    }
-    70% {
-      width: 249px;
-      height: 219px
-    }
-    100% {
-      width: 239px;
-      height: 209px
-    }
-  }
-
-  @-webkit-keyframes cloud5 {
-    0% {
-      width: 239px;
-      height: 209px
-    }
-    25% {
-      width: 249px;
-      height: 219px
-    }
-    50% {
-      width: 259px;
-      height: 229px
-    }
-    55% {
-      width: 259px;
-      height: 229px
-    }
-    70% {
-      width: 249px;
-      height: 219px
-    }
-    100% {
-      width: 239px;
-      height: 209px
-    }
-  }
-
-  @-moz-keyframes cloud5 {
-    0% {
-      width: 239px;
-      height: 209px
-    }
-    25% {
-      width: 249px;
-      height: 219px
-    }
-    50% {
-      width: 259px;
-      height: 229px
-    }
-    55% {
-      width: 259px;
-      height: 229px
-    }
-    70% {
-      width: 249px;
-      height: 219px
-    }
-    100% {
-      width: 239px;
-      height: 209px
-    }
-  }
-
-  @-o-keyframes cloud5 {
     0% {
       width: 239px;
       height: 209px
@@ -737,93 +408,8 @@
     top: 6%;
     left: 76%;
     animation: cloud6 5s infinite linear;
-    -moz-animation: cloud6 5s infinite linear;
-    -webkit-animation: cloud6 5s infinite linear;
-    -o-animation: cloud6 5s infinite linear;
   }
-
   @keyframes cloud6 {
-    0% {
-      transform: rotate(0deg);
-      left: 76%;
-    }
-    25% {
-      transform: rotate(20deg);
-      left: 76%;
-    }
-    50% {
-      transform: rotate(0deg);
-      left: 76%;
-    }
-    55% {
-      transform: rotate(0deg);
-      left: 76%;
-    }
-    70% {
-      transform: rotate(0deg);
-      left: 76%;
-    }
-    100% {
-      transform: rotate(0deg);
-      left: 76%;
-    }
-  }
-
-  @-webkit-keyframes cloud6 {
-    0% {
-      transform: rotate(0deg);
-      left: 76%;
-    }
-    25% {
-      transform: rotate(20deg);
-      left: 76%;
-    }
-    50% {
-      transform: rotate(0deg);
-      left: 76%;
-    }
-    55% {
-      transform: rotate(0deg);
-      left: 76%;
-    }
-    70% {
-      transform: rotate(0deg);
-      left: 76%;
-    }
-    100% {
-      transform: rotate(0deg);
-      left: 76%;
-    }
-  }
-
-  @-moz-keyframes cloud6 {
-    0% {
-      transform: rotate(0deg);
-      left: 76%;
-    }
-    25% {
-      transform: rotate(20deg);
-      left: 76%;
-    }
-    50% {
-      transform: rotate(0deg);
-      left: 76%;
-    }
-    55% {
-      transform: rotate(0deg);
-      left: 76%;
-    }
-    70% {
-      transform: rotate(0deg);
-      left: 76%;
-    }
-    100% {
-      transform: rotate(0deg);
-      left: 76%;
-    }
-  }
-
-  @-o-keyframes cloud6 {
     0% {
       transform: rotate(0deg);
       left: 76%;
