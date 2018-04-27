a6fe6ca9264512ef49917f93a834536d396b3cda
晏立 <fengwu2006@gmail.com>
2018-04-27 16:27:55 +0800

    save

--- src/mock/permissiontree.js
+++ src/mock/permissiontree.js
@@ -15,38 +15,145 @@ function createItem(treeId, parentId) {
   return item
 }
 
-permissions.push(createItem(1, -1))
-permissions.push(createItem(2, -1))
-permissions.push(createItem(3, -1))
+let app = [
+  {
+    "treeId": "300000",
+    "text": "工作台",
+    "parentId": null,
+    "systemType": " APP"
+  },
+  {
+    "treeId": "301100",
+    "text": "业主查询",
+    "parentId": "300000",
+    "systemType": " APP"
+  },
+  {
+    "treeId": "301200",
+    "text": "信息上报",
+    "parentId": "300000",
+    "systemType": " APP"
+  },
+  {
+    "treeId": "301300",
+    "text": "上报记录",
+    "parentId": "300000",
+    "systemType": " APP"
+  },
+  {
+    "treeId": "301400",
+    "text": "车辆盘点",
+    "parentId": "300000",
+    "systemType": " APP"
+  },
+  {
+    "treeId": "301500",
+    "text": "车辆收费",
+    "parentId": "300000",
+    "systemType": " APP"
+  },
+  {
+    "treeId": "301600",
+    "text": "授权审批",
+    "parentId": "300000",
+    "systemType": " APP"
+  },
+  {
+    "treeId": "301700",
+    "text": "经营分析",
+    "parentId": "300000",
+    "systemType": " APP"
+  },
+  {
+    "treeId": "301710",
+    "text": "车场收费资金流统计",
+    "parentId": "301700",
+    "systemType": " APP"
+  },
+  {
+    "treeId": "301720",
+    "text": "业主报修统计",
+    "parentId": "301700",
+    "systemType": " APP"
+  },
+  {
+    "treeId": "301730",
+    "text": "业主投诉统计",
+    "parentId": "301700",
+    "systemType": " APP"
+  },
+  {
+    "treeId": "301740",
+    "text": "卡务资金流统计",
+    "parentId": "301700",
+    "systemType": " APP"
+  },
+  {
+    "treeId": "301750",
+    "text": "社区人流量统计",
+    "parentId": "301700",
+    "systemType": " APP"
+  },
+  {
+    "treeId": "301760",
+    "text": "车场异常事件统计",
+    "parentId": "301700",
+    "systemType": " APP"
+  },
+  {
+    "treeId": "301770",
+    "text": "卡片活跃度统计",
+    "parentId": "301700",
+    "systemType": " APP"
+  },
+  {
+    "treeId": "301780",
+    "text": "车场消费打折统计",
+    "parentId": "301700",
+    "systemType": " APP"
+  },
+  {
+    "treeId": "310000",
+    "text": "任务",
+    "parentId": null,
+    "systemType": " APP"
+  },
+  {
+    "treeId": "311100",
+    "text": "我的派工",
+    "parentId": "310000",
+    "systemType": " APP"
+  }
+]
 
-permissions.push(createItem(11, 1))
-permissions.push(createItem(12, 1))
-permissions.push(createItem(13, 1))
-
-permissions.push(createItem(21, 2))
-permissions.push(createItem(22, 2))
-permissions.push(createItem(23, 2))
-
-permissions.push(createItem(31, 3))
-permissions.push(createItem(32, 3))
-
-permissions.push(createItem(41, 11))
-permissions.push(createItem(42, 11))
-
-permissions.push(createItem(51, 12))
-permissions.push(createItem(52, 12))
-
-console.log('zz')
+let icop = [
+  {
+    "treeId": "100000",
+    "text": "系统管理",
+    "parentId": null,
+    "systemType": "ICOP"
+  },
+  {
+    "treeId": "101000",
+    "text": "用户管理",
+    "parentId": "100000",
+    "systemType": "ICOP"
+  },
+  {
+    "treeId": "101100",
+    "text": "新增用户",
+    "parentId": "101000",
+    "systemType": "ICOP"
+  }
+]
 
 export default {
   
   queryRolePopedom: config => {
     
-    console.log('zzz')
-    
     return {
       data: {
-        app:[], icop:[]
+        app:app, icop:icop
       },
     }
   }
