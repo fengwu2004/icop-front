7b90a682a2984538ccd77c2fa3d9193bcb2a416b
晏立 <fengwu2006@gmail.com>
2018-05-06 10:58:07 +0800

    save

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 
 import 'normalize.css/normalize.css'// A modern alternative to CSS resets
 
+import '@/styles/globalstyle.scss'
+
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
