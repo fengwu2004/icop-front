d91c069911ed8351f3eccc5fa383b37fcd79d0e3
晏立 <fengwu2006@gmail.com>
2018-05-08 12:03:04 +0800

    save

--- src/views/layout/components/Sidebar/SidebarItem.vue
+++ src/views/layout/components/Sidebar/SidebarItem.vue
@@ -1,17 +1,16 @@
 <template>
   <div>
-    <template v-for="item in routes" v-if="!item.hidden&&item.children">
+    <template v-for="item in routes" v-if="!item.hidden && item.children">
       <el-submenu :index="getMenuIndex(item)" :key="item.name">
         <template slot="title">
-          <svg-icon v-if="item.meta&&item.meta.icon" :icon-class="item.meta.icon"></svg-icon>
-          <span v-if="item.meta&&item.meta.title" slot="title" style="color:#16325C">{{ generateTitle(item.meta.title) }}</span>
+          <span v-if="item.meta && item.meta.title" slot="title" style="color:#445577">{{ generateTitle(item.meta.title) }}</span>
         </template>
         <template v-for="(child, index) in item.children" v-if="!child.hidden">
           <sidebar-item :is-nest="true" class="nest-menu" v-if=" child.children && child.children.length > 0 && hasOneShowingChildren(child.children)" :routes="[child]" :key="child.path"></sidebar-item>
           <router-link v-else :to="composePath(item, child)" :key="child.name">
             <el-menu-item :index="composePath(item, child)">
-              <svg-icon v-if="child.meta&&child.meta.icon" :icon-class="child.meta.icon"></svg-icon>
-              <span v-if="child.meta&&child.meta.title" slot="title" style="color:#16325C">{{ generateTitle(child.meta.title) }}</span>
+              <svg-icon v-if="child.meta && child.meta.icon" :icon-class="child.meta.icon"></svg-icon>
+              <span v-if="child.meta && child.meta.title" slot="title" style="color:#16325C">{{ generateTitle(child.meta.title) }}</span>
             </el-menu-item>
           </router-link>
         </template>
--- src/views/layout/components/Sidebar/index.vue
+++ src/views/layout/components/Sidebar/index.vue
@@ -1,15 +1,7 @@
 <template>
   <scroll-bar>
-    <el-menu
-      mode="vertical"
-      :show-timeout="200"
-      :default-active="$route.path"
-      :collapse="isCollapse"
-      background-color="#e0e5ee"
-      text-color="#bfcbd9"
-      active-text-color="red"
-      :default-openeds="defaultactiveindex"
-    >
+    <el-menu mode="vertical" :show-timeout="200" :default-active="$route.path"
+      :collapse="isCollapse" background-color="#e0e5ee" text-color="#bfcbd9" active-text-color="#FFFFFF" :default-openeds="defaultactiveindex">
       <sidebar-item :routes="permission_routers"></sidebar-item>
     </el-menu>
   </scroll-bar>
