61f969da51653ae98d18df715e214a509510e734
晏立 <fengwu2006@gmail.com>
2018-05-09 16:47:13 +0800

    save

--- src/views/systemadmin/user/edit.vue
+++ src/views/systemadmin/user/edit.vue
@@ -18,11 +18,11 @@
           </div>
         </div>
         <div class="itemrow">
-          <div style="margin-top: 2rem">
+          <div>
             <span>姓名</span>
             <el-input style="margin-top: 0.5rem" :disabled="justEnableSelect" v-model="currentEditUser.name"></el-input>
           </div>
-          <div style="margin-top: 2rem">
+          <div>
             <span>性别</span>
             <div style="margin-top: 0.5rem">
               <el-radio :disabled="justEnableSelect" v-model="currentEditUser.sex" label="MALE">男</el-radio>
@@ -31,14 +31,14 @@
           </div>
         </div>
         <div class="itemrow">
-          <div style="margin-top: 2rem">
+          <div>
             <span class="redstar">*</span><span class="sumtitle">账户</span>
             <el-input style="margin-top: 0.5rem" v-model="currentEditUser.userName" @blur="onUserNameBlur"></el-input>
             <div style="margin-top: 10px">
               <span style="font-size: 0.8rem;color: #445577;">注:不能重名,登录时需同时输入@部分</span>
             </div>
           </div>
-          <div style="margin-top: 3rem">
+          <div>
             <span class="redstar">*</span><span class="sumtitle">密码</span>
             <input style="display:none" name="txtpwd">
             <el-input type="password" style="margin-top: 0.5rem" placeholder="******" v-model="currentEditUser.password" name="txtpwd"></el-input>
@@ -232,7 +232,13 @@
 
     display: flex;
     justify-content: space-between;
-    
+    width: 80%;
+    margin: 2rem auto;
+  }
+
+  .itemrow > div {
+
+    width: 40%;
   }
 
   .specialinput {
