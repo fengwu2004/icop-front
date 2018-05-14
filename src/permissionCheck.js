8ce00fc4d3a70c1d10b78d0acbedd54ba177a315
晏立 <fengwu2006@gmail.com>
2018-05-14 19:02:39 +0800

    save

--- src/permissionCheck.js
+++ src/permissionCheck.js
@@ -9,7 +9,7 @@ export function checkRouteAndActionEnable(code) {
   
   for (let i = 0; i < store.getters.permissioncodes.length; ++i) {
     
-    if (code === store.getters.permissioncodes[i]) {
+    if (code == store.getters.permissioncodes[i]) {
       
       return true
     }
--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -10,7 +10,7 @@ function checkRouteAndActionEnable(code) {
   
   for (let i = 0; i < store.getters.permissioncodes.length; ++i) {
     
-    if (code === store.getters.permissioncodes[i]) {
+    if (code == store.getters.permissioncodes[i]) {
       
       return true
     }
