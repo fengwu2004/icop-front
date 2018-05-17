e058684ec7a59cc7e77e4aa957513f106d841360
Unknown <fengwu2006@gmail.com>
2018-05-17 17:34:16 +0800

    save

--- src/utils/request.js
+++ src/utils/request.js
@@ -6,7 +6,7 @@ import { getTokenAndId, checkValidTokenAndUserId } from '@/utils/auth'
 // create an axios instance
 const service = axios.create({
   baseURL: process.env.BASE_API, // api的base_url
-  timeout: 5000 // request timeout
+  timeout: 30000 // request timeout
 })
 
 service.interceptors.request.use(config => {
